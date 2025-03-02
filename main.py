78a0c6b04bc7decc69180cbecb2f6353db3fff08
Yunus Taşyürek <126098870+SaikaTheGreat@users.noreply.github.com>
2025-03-02 16:29:55 +0300

    dict

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 def int_to_roman(num):
+    roman_dict = {1000 : "M", 900 : "CM", 500 : "D", 400 : "CD", 100 : "C", 90 : "XC", 50 : "L"
+              , 40 : "XL", 10 : "X", 9 : "IX", 5 : "V", 4 : "IV", 1 : "I"}
+
+
     """
     Convert an integer to a Roman numeral.
 
