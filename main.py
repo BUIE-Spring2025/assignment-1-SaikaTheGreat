05a19c9cc5810e82e5922ccdf5c8a641cac7cb16
Yunus Taşyürek <126098870+SaikaTheGreat@users.noreply.github.com>
2025-03-02 16:49:09 +0300

    bug

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def int_to_roman(num):
     roman_dict = {1000 : "M", 900 : "CM", 500 : "D", 400 : "CD", 100 : "C", 90 : "XC", 50 : "L"
               , 40 : "XL", 10 : "X", 9 : "IX", 5 : "V", 4 : "IV", 1 : "I"}
     numeros = ""
-    for i, j in roman_dict:
+    for i, j in roman_dict.items():
         while num >= i:
             numeros += j
             num -= i
